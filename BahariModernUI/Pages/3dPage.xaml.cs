78a1708598d53d159ef2dddc85acde7f9e4a1265
santensuru <djuned.ong@gmail.com>
2015-04-11 02:00:38 +0700

    3D Model
    Can rotate left/right

--- BahariModernUI/Pages/3dPage.xaml.cs
+++ BahariModernUI/Pages/3dPage.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -24,6 +25,9 @@ namespace BahariModernUI.Pages
     /// </summary>
     public partial class MapPage : UserControl
     {
+
+        AxisAngleRotation3D ax3d;
+            
         public MapPage()
         {
             InitializeComponent();
@@ -39,13 +43,18 @@ namespace BahariModernUI.Pages
 
             HelixToolkit.Wpf.ObjReader CurrentHelixObjReader = new HelixToolkit.Wpf.ObjReader();
 
-            Uri uri = new Uri("C:/Users/user/Downloads/Compressed/jelly fish series jelly fish one/jellyfish series jelly fish one improved.obj", UriKind.Relative);
+            Uri uri = new Uri("C:/Users/user/Downloads/Compressed/Tuna/Tuna/TUNA.obj", UriKind.Relative);
+            //Uri uri = new Uri("C:/Users/user/Downloads/Compressed/jelly fish series jelly fish one/jellyfish series jelly fish one improved.obj", UriKind.Relative);
             //ModernDialog.ShowMessage(uri.ToString(), "", MessageBoxButton.OK);
 
             System.Windows.Media.Media3D.Model3DGroup MyModel = CurrentHelixObjReader.Read(uri.ToString());
 
             // Display the model
             foo.Content = MyModel;
+
+            ax3d = new AxisAngleRotation3D(new Vector3D(0, 2, 0), 1);
+            RotateTransform3D myRotateTransform = new RotateTransform3D(ax3d);
+            foo.Transform = myRotateTransform;
 
             //foo.Transform.
 
@@ -66,5 +75,28 @@ namespace BahariModernUI.Pages
             }
         }
 
+        private void Left_Button(object sender, RoutedEventArgs e)
+        {
+            ax3d.Angle += 30;
+            ax3d.Angle %= 360;
+        }
+
+        private void Right_Button(object sender, RoutedEventArgs e)
+        {
+            ax3d.Angle -= 30;
+            ax3d.Angle %= 360;
+        }
+
+        private void Left_RepeatButton(object sender, RoutedEventArgs e)
+        {
+            ax3d.Angle += 10;
+            ax3d.Angle %= 360;
+        }
+
+        private void Right_RepeatButton(object sender, RoutedEventArgs e)
+        {
+            ax3d.Angle -= 10;
+            ax3d.Angle %= 360;
+        }
     }
 }
